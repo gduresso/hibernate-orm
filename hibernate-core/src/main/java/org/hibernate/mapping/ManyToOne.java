72ab7b17b513336876b2eb006292a43ed2f40216
Brett Meyer <brett@3riverdev.com>
2013-05-15 15:44:17 -0400

    HHH-6813 @Id @OneToOne cause NullPointerException during query

--- hibernate-core/src/main/java/org/hibernate/mapping/ManyToOne.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/ManyToOne.java
@@ -45,7 +45,8 @@ public class ManyToOne extends ToOne {
 
 	public Type getType() throws MappingException {
 		return getMappings().getTypeResolver().getTypeFactory().manyToOne(
-				getReferencedEntityName(), 
+				getReferencedEntityName(),
+				true, 
 				getReferencedPropertyName(),
 				isLazy(),
 				isUnwrapProxy(),
